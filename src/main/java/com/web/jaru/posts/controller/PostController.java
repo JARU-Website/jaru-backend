ef2b644ce5cfc780ccf42fedf6d4765b3044e019
Leeyoujin00 <cymn6032@gmail.com>
2025-08-31 18:49:37 +0900

    [REFACTOR] 게시글 목록 조회 API 수정
    PathVariable를 RequestParam으로 변경

--- src/main/java/com/web/jaru/posts/controller/PostController.java
+++ src/main/java/com/web/jaru/posts/controller/PostController.java
@@ -34,15 +34,15 @@ public class PostController {
     }
 
     // 게시글 목록 조회(최신순)
-    @GetMapping("/list/{postCategoryId}/{certCategoryId}")
-    public ApiResponse<PageDto<PostResponse.Summary>> findNewestPostList(@PathVariable("postCategoryId") Long postCategoryId, @PathVariable(name = "certCategoryId", required = false) Long certCategoryId,
+    @GetMapping("/list")
+    public ApiResponse<PageDto<PostResponse.Summary>> findNewestPostList(@RequestParam Long postCategoryId, @RequestParam(required = false) Long certCategoryId,
                                                                          @PageableDefault(page = 0, size = 10)  Pageable pageable) {
         return ApiResponse.onSuccess(postService.findNewest(postCategoryId, certCategoryId, pageable), SuccessCode.OK);
     }
 
     // 게시글 목록 조회(추천순)
-    @GetMapping("/list/{postCategoryId}/{certCategoryId}")
-    public ApiResponse<PageDto<PostResponse.Summary>> findMostLikedPostList(@PathVariable("postCategoryId") Long postCategoryId, @PathVariable(name = "certCategoryId", required = false) Long certCategoryId,
+    @GetMapping("/list")
+    public ApiResponse<PageDto<PostResponse.Summary>> findMostLikedPostList(@RequestParam Long postCategoryId, @RequestParam(required = false) Long certCategoryId,
                                                                             @PageableDefault(page = 0, size = 10)  Pageable pageable) {
         return ApiResponse.onSuccess(postService.findNewest(postCategoryId, certCategoryId, pageable), SuccessCode.OK);
     }
